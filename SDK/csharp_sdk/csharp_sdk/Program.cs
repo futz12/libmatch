be235d6da691573b16856382322c1901448cbac1
66hh <49398720+66hh@users.noreply.github.com>
2024-08-06 21:36:51 +0800

    整理sdk目录以及增加csharp sdk

---
+++ SDK/csharp_sdk/csharp_sdk/Program.cs
@@ -0,0 +1,32 @@
+﻿using LibMatch;
+using System.Runtime.InteropServices;
+
+namespace csharp_sdk
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Version: " + NativeApi.Version);
+
+            byte[] Template = File.ReadAllBytes("eye.png");
+            byte[] Source = File.ReadAllBytes("main.png");
+            TemplateMatcher matcher = new TemplateMatcher(Template, MatchMode.COLOR_BGR);
+            List<ObjectEx> result = matcher.Compute(Source);
+            Console.WriteLine("Result: " + result.Count);
+            for (int i = 0; i < result.Count; i++)
+            {
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine("prob: " + result[i].prob.ToString());
+                Console.WriteLine("rect:");
+                Console.WriteLine("    x: " + result[i].rect.x);
+                Console.WriteLine("    y: " + result[i].rect.y);
+                Console.WriteLine("    width: " + result[i].rect.width);
+                Console.WriteLine("    height :" + result[i].rect.height);
+                Console.WriteLine("-----------------------------------------------------------");
+            }
+            Console.ReadLine();
+        }
+    }
+}
